bfe934e378a52953b30eef68810fcff3cfc14e04
Abdufattoxov <abdufattoxovshaxzod0404@gmail.com>
2024-12-16 12:02:06 +0500

    initial commit

--- Cards/src/main/java/uz/shaxzod/cards/entity/BaseEntity.java
+++ Cards/src/main/java/uz/shaxzod/cards/entity/BaseEntity.java
@@ -1,4 +1,36 @@
 package uz.shaxzod.cards.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class BaseEntity {
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(updatable = false)
+    private String updatedBy;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime updatedAt;
 }
--- Cards/src/main/java/uz/shaxzod/cards/repository/CardsRepository.java
+++ Cards/src/main/java/uz/shaxzod/cards/repository/CardsRepository.java
@@ -1,4 +1,16 @@
 package uz.shaxzod.cards.repository;
 
-public class CardsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.shaxzod.cards.entity.Cards;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
 }
